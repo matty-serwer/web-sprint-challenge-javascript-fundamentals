62e5c4286cec2e5b13b67a876bc579ea7c37ad21
Matthew Serwer <Matthew.Serwer@gmail.com>
2020-09-18 12:18:25 -0400

    completed arrays, prototype, and classes MVP tasks

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,25 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
+/* 
+A nested function can use closure to access variables outside of itself, in the greater function scope, when the variable is not found within the nested function.
+*/
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(num) {
+  let count = 0;
+  return function counter(num) {
+    // for(let i = 0; i <= num; i++){
+    //   count += num;
+    //   num--;
+    // }
+    return count++;
+  };
+}
+
+const test = summation(4);
+console.log(test);
